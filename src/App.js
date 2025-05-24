7271be6fb710a8ea300c8d7c693f5566d8f78712
Talha Aslam <143254576+talhaaslam08@users.noreply.github.com>
2025-05-24 11:27:32 +0500

    Added files in src folder

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// Copy the entire component code from the artifact above
+// This is your main application component
+import React from 'react';
+import CafeManagementSystem from './components/CafeManagementSystem';
+
+function App() {
+  return (
+    <div className="App">
+      <CafeManagementSystem />
+    </div>
+  );
+}
+
+export default App;
